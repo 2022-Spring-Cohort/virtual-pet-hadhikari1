882533b84c68b18f5447cb7f3326f12f9a87667e
hadhikari1 <adhikarihari0@gmail.com>
2022-01-22 18:30:38 -0500

    completed the test cases and got the project working

--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,9 +1,77 @@
 package virtual_pet;
 
+import java.util.Scanner;
+
+import java.util.Random;
+
 public class VirtualPetApplication {
 
     public static void main(String[] args) {
-        //Interact with a VirtualPet object in this method
-    }
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Virtual Pet Game.");
+        System.out.println("Please enter your pet's name");
+        String petName = input.nextLine();
 
+        VirtualPet pet = new VirtualPet(petName);
+        System.out.println("Hi, my name is " + petName + ".");
+
+        Random rand = new Random();
+
+        int chosenNumber;
+
+        do{
+            System.out.println("Choose a number to play" + "\n Press 0 to Quit." + "\n Press 1 to Feed "
+                    + petName + "." + "\n Press 2 to Play with " + petName + "." +"\n Press 3 to Let "+ petName
+                    + " Sleep." + "\n Press 4 to Change Time.");
+
+            chosenNumber = input.nextInt();
+            if(chosenNumber == 0){
+                System.out.println("Bye, See you next time.");
+                break;
+            }
+            else if(chosenNumber == 1){
+                pet.feedPet();
+                System.out.println(pet.getPetStatus());
+            }
+            else if(chosenNumber == 2){
+                pet.playTime();
+                System.out.println(pet.getPetStatus());
+            }
+            else if(chosenNumber == 3){
+                pet.napTime();
+                System.out.println(pet.getPetStatus());
+            }
+            else if(chosenNumber == 4){
+                pet.tick();
+                System.out.println(pet.getPetStatus());
+            }
+
+            if (pet.getHunger() >= 20) {
+                System.out.println("Sorry your pet died of hunger.");
+                break;
+            }
+            else if (pet.getHunger() <= 0) {
+                System.out.println("Sorry your pet died due to overfeeding.");
+                break;
+            }
+            if (pet.getSleepy() >= 20) {
+                System.out.println("Sorry your pet died due to lack of sleep.");
+                break;
+            }
+            else if (pet.getSleepy() <= 0) {
+                System.out.println("Sorry your pet died due to over resting.");
+                break;
+            }
+            if (pet.getBoredom() >= 20) {
+                System.out.println("Sorry your pet died of boredom.");
+                break;
+            }
+            else if (pet.getBoredom() <= 0) {
+                System.out.println("Sorry your pet died due to too much play time.");
+                break;
+            }
+        }
+        while(chosenNumber !=0);
+    }
 }
+
--- src/test/java/virtual_pet/VirtualPetTest.java
+++ src/test/java/virtual_pet/VirtualPetTest.java
@@ -1,3 +1,52 @@
 package virtual_pet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class VirtualPetTest {
+    @Test
+    public void getNameTest() {
+      VirtualPet pet = new VirtualPet("Whiskers");
+      String actual = pet.getName();
+      assertEquals("Whiskers", actual);
+    }
+    @Test
+    public void feedPetTest() {
+        VirtualPet pet = new VirtualPet("Whiskers");
+        pet.feedPet();
+        assertEquals(2,pet.getHunger());
+        assertEquals(4,pet.getBoredom());
+        assertEquals(4,pet.getSleepy());
+    }
+    @Test
+    public void playTimeTest() {
+        VirtualPet pet = new VirtualPet("Whiskers");
+        pet.playTime();
+        assertEquals(4,pet.getHunger());
+        assertEquals(4,pet.getSleepy());
+        assertEquals(2,pet.getBoredom());
+    }
+    @Test
+    public void napTimeTest() {
+        VirtualPet pet = new VirtualPet("Whiskers");
+        pet.napTime();
+        assertEquals(4,pet.getBoredom());
+        assertEquals(4,pet.getHunger());
+        assertEquals(2,pet.getSleepy());
+    }
+    @Test
+    public void tickTest(){
+        VirtualPet pet = new VirtualPet("Whiskers");
+        pet.tick();
+        assertEquals(8,pet.getHunger());
+        assertEquals(8,pet.getBoredom());
+        assertEquals(8,pet.getSleepy());
+    }
+//    @Test
+//    public void getPetStatus() {
+//        VirtualPet pet = new VirtualPet("Whiskers");
+//        pet.getPetStatus();
+//
+//    }
 }
